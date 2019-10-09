6ce8edfd7fed4553ff70e43ee5348e9bb3aedaef
Midhun PC <midhun.techversant@gmail.com>
2019-10-09 10:40:14 +0530

    django part removed

--- bmiapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BmiappConfig(AppConfig):
-    name = 'bmiapp'
--- bmiapp/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.views.generic.base import View, TemplateView
-# Create your views here.
-
-
-class HomePageView(TemplateView):
-    template_name = 'index.html'
